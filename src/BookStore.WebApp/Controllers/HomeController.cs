a57c76bab5937b94c2e3d3a9c9dbe3696c628b1c
Elladiel <nastyazubastya1999@mail.ru>
2017-11-04 01:04:49 +0300

    initial

---
+++ src/BookStore.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.WebApp.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace BookStore.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.GetAsync("http://localhost:55328/api/books/").Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            var books = JsonConvert.DeserializeObject<List<Book>>(json);
+
+            return View(books);
+        }
+    }
+}
