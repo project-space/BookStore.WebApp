1d8f950a17cc1beed059fe0afd0f1ad2441e1f0f
Elladiel <nastyazubastya1999@mail.ru>
2018-01-01 22:53:12 +0300

    cleaned code

--- src/BookStore.WebApp/Controllers/GenreController.cs
+++ src/BookStore.WebApp/Controllers/GenreController.cs
@@ -1,7 +1,4 @@
 ﻿using BookStore.WebApp.Models;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Net.Http;
 using System.Web.Mvc;
 using BookStore.WebApp.Mappers;
 using BookStore.Common.BookServiceClient;
@@ -24,22 +21,5 @@ namespace BookStore.WebApp.Controllers
             return View(model);
         }
 
-        private List<Book> GetBooks(string action)
-        {
-            var httpClient = new HttpClient();
-            var response = httpClient.GetAsync($"http://localhost:55328/api/books/{action}").Result;
-            var json = response.Content.ReadAsStringAsync().Result;
-
-            return JsonConvert.DeserializeObject<List<Book>>(json);
-        }
-
-        private Genre GetGenre(string action)
-        {
-            var httpClient = new HttpClient();
-            var response = httpClient.GetAsync($"http://localhost:55328/api/genres/{action}").Result;
-            var json = response.Content.ReadAsStringAsync().Result;
-
-            return JsonConvert.DeserializeObject<Genre>(json);
-        }
     }
 }
