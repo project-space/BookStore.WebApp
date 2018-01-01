9433af6f459f4c7652458afe34d4c7acee64ebcc
Elladiel <nastyazubastya1999@mail.ru>
2018-01-01 23:10:28 +0300

    add references

---
+++ src/BookStore.WebApp/Scripts/Menu.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    if ($.cookie('cart') == null) {
+        $.get('/Cart/Get', function (data) {
+            $.cookie('cart', JSON.stringify(data));
+            $('<a>', { href: '/Cart/Index/' + data.Id, text: 'Корзина' }).appendTo('#cart');
+        });
+    } else {
+        var cart = JSON.parse($.cookie('cart'));
+        $('<a>', { href: '/Cart/Index/' + cart.Id, text: 'Корзина' }).appendTo('#cart');
+    }
+});
