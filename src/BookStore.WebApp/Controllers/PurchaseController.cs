e9e95b12faab8b4bcbf71a3f3ec1c951c7f50879
Elladiel <nastyazubastya1999@mail.ru>
2018-01-16 10:47:22 +0300

    added the ability to make order

---
+++ src/BookStore.WebApp/Controllers/PurchaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.WebApp.Models;
+
+namespace BookStore.WebApp.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Checkout(Order order)
+        {
+
+        }
+    }
+}
---
+++ src/BookStore.WebApp/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.WebApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public List<int> BookIds { get; set; }
+        public string GuestId { get; set; }
+        public string FullName { get; set; }
+        public string Postcode { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
