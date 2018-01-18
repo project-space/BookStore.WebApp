c30af98820897fba771c89ea66b5b1137b14e871
Elladiel <nastyazubastya1999@mail.ru>
2018-01-18 21:46:52 +0300

    created order mapper

---
+++ src/BookStore.WebApp/Mappers/OrderMapper.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Common.PurchaseServiceClient.Models;
+using BookStore.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.WebApp.Mappers
+{
+    public class OrderMapper
+    {
+        public static Common.PurchaseServiceClient.Models.Order Map(Models.Order order)
+        {
+            return new Common.PurchaseServiceClient.Models.Order
+            {
+                Id = order.Id,
+                AccountId = order.AccountId,
+                BookIds = order.BookIds,
+                GuestId = order.GuestId,
+                FullName = order.FullName,
+                Postcode = order.Postcode,
+                Country = order.Country,
+                City = order.City,
+                Street = order.Street,
+                House = order.House,
+                Apartment = order.Apartment,
+                PhoneNumber = order.PhoneNumber
+            };
+        }
+
+        public static List<Common.PurchaseServiceClient.Models.Order> Map(List<Models.Order> orders)
+        {
+            return orders.ConvertAll(Map);
+        }
+    }
+}
