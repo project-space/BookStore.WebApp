4eba36f0ab3d3c573de5fb0c73c9f6fbabdd804d
nasty <nasty@DESKTOP-5UP85BN>
2018-02-20 13:42:13 +0300

    nuget packages was updated

--- src/BookStore.WebApp/Controllers/HomeController.cs
+++ src/BookStore.WebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Mvc;
 using BookStore.WebApp.Mappers;
 using BookStore.Common.ApiClients.Design.Abstractions.BookServiceClient;
+using System.Threading.Tasks;
 
 namespace BookStore.WebApp.Controllers
 {
@@ -18,10 +19,10 @@ namespace BookStore.WebApp.Controllers
         }
 
         [HttpGet]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            var novelties = BookMapper.Map(booksClient.GetNovelties().Result);
-            var popular = BookMapper.Map(booksClient.GetPopular().Result);
+            var novelties = BookMapper.Map(await booksClient.GetNovelties());
+            var popular = BookMapper.Map(await booksClient.GetPopular());
 
             var model = new MainContentModel { Novelties = novelties, Popular = popular};
             
